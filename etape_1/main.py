025f16a23baa99dfa0d223a9eb7daa5a427eb418
naoufelboukri <naoufelboukri@gmail.com>
2023-11-15 15:40:40 +0100

    initialized project

---
+++ etape_1/main.py
@@ -0,0 +1,26 @@
+import cv2 as cv
+import PySimpleGUI as sg
+
+im = cv.imread('./imgs/licorne.png')
+assert im is not None, "file could not be read, check with os.path.exists()"
+imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
+ret, thresh = cv.threshold(imgray, 127, 255, 0)
+contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+cv.drawContours(im, contours, -1, (0,255,0), 3)
+
+sg.theme('DarkAmber')
+layout = [  [sg.Text('Some text on Row 1')],
+            [sg.Text('Enter something on Row 2'), sg.InputText()],
+            [sg.Button('Ok'), sg.Button('Cancel')] ]
+
+# Create the Window
+window = sg.Window('Window Title', layout)
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+    print('You entered ', values[0])
+
+window.close()
