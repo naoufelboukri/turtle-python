554de46e8a330ef1d93bf044fa0008ad8e41ce61
aggoun_r <aggoun_r@etna-alternance.net>
2023-12-06 15:21:28 +0100

    Update main.py

--- etape_1/main.py
+++ etape_1/main.py
@@ -1,12 +1,21 @@
 import cv2
+import numpy as np
 
-pathFile = './../imgs/licorne.png'
+pathFile = './../imgs/nuage.jpg'
 
 image = cv2.imread(pathFile)
 
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred_img = cv2.blur(image,ksize=(5,5))
 
-edges = cv2.Canny(gray_image,130,290)
+gray_image = cv2.cvtColor(blurred_img, cv2.COLOR_BGR2GRAY)
+
+median_pix = np.median(gray_image)
+
+lower = int(max(0 ,0.25*median_pix))
+upper = int(min(255,0.75*median_pix))
+
+edges = cv2.Canny(image=blurred_img, threshold1=lower,threshold2=upper)
+
 
 ret, thresh = cv2.threshold(edges, 127, 255, cv2.THRESH_BINARY_INV)
 
