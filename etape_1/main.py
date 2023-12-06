91f63512055bf7d686744d2851a0d9476ea9744b
aggoun_r <aggoun_r@etna-alternance.net>
2023-12-06 11:54:33 +0100

    Update main.py

--- etape_1/main.py
+++ etape_1/main.py
@@ -1,26 +1,11 @@
-import cv2 as cv
-import PySimpleGUI as sg
+import cv2
 
-im = cv.imread('./imgs/licorne.png')
-assert im is not None, "file could not be read, check with os.path.exists()"
-imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
-ret, thresh = cv.threshold(imgray, 127, 255, 0)
-contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+image = cv2.imread('./imgs/licorne.png')
 
-cv.drawContours(im, contours, -1, (0,255,0), 3)
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-sg.theme('DarkAmber')
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+edges = cv2.Canny(gray_image,130,290)
 
-# Create the Window
-window = sg.Window('Window Title', layout)
-# Event Loop to process "events" and get the "values" of the inputs
-while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
-        break
-    print('You entered ', values[0])
+ret, thresh = cv2.threshold(edges, 127, 255, cv2.THRESH_BINARY_INV)
 
-window.close()
+cv2.imwrite('etape1.jpg', thresh)
