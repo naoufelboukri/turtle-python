e3834e8764577ddcff7f75c24e179fc3cdfd68e2
aggoun_r <aggoun_r@etna-alternance.net>
2023-12-23 13:38:03 +0100

    Update main.py

--- etape_1/main.py
+++ etape_1/main.py
@@ -1,22 +1,21 @@
 import cv2
 import numpy as np
+import sys
 
-pathFile = './../imgs/lion.jpg'
+def big_fernand(pathFile): 
+    image = cv2.imread(pathFile)
+    blurred_img = cv2.blur(image,ksize=(5,5))
+    gray_image = cv2.cvtColor(blurred_img, cv2.COLOR_BGR2GRAY)
+    median_pix = np.median(gray_image)
+    lower = int(max(0 ,0.25*median_pix))
+    upper = int(min(255,0.75*median_pix))
+    edges = cv2.Canny(image=blurred_img, threshold1=lower,threshold2=upper)
+    ret, thresh = cv2.threshold(edges, 127, 255, cv2.THRESH_BINARY_INV)
+    cv2.imwrite('etape1.jpg', thresh)
 
-image = cv2.imread(pathFile)
-
-blurred_img = cv2.blur(image,ksize=(5,5))
-
-gray_image = cv2.cvtColor(blurred_img, cv2.COLOR_BGR2GRAY)
-
-median_pix = np.median(gray_image)
-
-lower = int(max(0 ,0.25*median_pix))
-upper = int(min(255,0.75*median_pix))
-
-edges = cv2.Canny(image=blurred_img, threshold1=lower,threshold2=upper)
+def main():
+    if len(sys.argv) > 1:
+        big_fernand(sys.argv[1])
 
 
-ret, thresh = cv2.threshold(edges, 127, 255, cv2.THRESH_BINARY_INV)
-
-cv2.imwrite('etape1.jpg', thresh)
+main()
