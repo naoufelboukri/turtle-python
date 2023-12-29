3745d8c158c18c26aa79d0b6cbd5b47f6214b5cc
abdou <sani_a@etna-alternance.net>
2023-12-29 21:03:42 +0100

    etape 3 drawing

--- etape_3/main.py
+++ etape_3/main.py
@@ -1,27 +1,47 @@
 import cv2
 import numpy as np
 import sys
+import turtle
 
 def big_fernand_with_blur(pathFile, intensity):
+    turtle.speed(0)
     image = cv2.imread(pathFile)
+    rows, columns, channels = image.shape
+    turtle.setup(columns, rows)
     blurred_img = cv2.blur(image,ksize=(intensity, intensity))
     gray_image = cv2.cvtColor(blurred_img, cv2.COLOR_BGR2GRAY)
     median_pix = np.median(gray_image)
-    lower = int(max(0 ,0.25*median_pix))
+    lower = max(0 ,0.25*median_pix)
     upper = int(min(255,0.75*median_pix))
     edges = cv2.Canny(image=blurred_img, threshold1=lower,threshold2=upper)
     ret, thresh = cv2.threshold(edges, 127, 255, cv2.THRESH_BINARY_INV)
-    cv2.imwrite('etape2.jpg', thresh)
+    for y in range(rows):
+      for x in range(columns):
+        pixel = float(thresh[y, x])
+        turtle.penup()
+        turtle.goto(x, rows - y)
+        turtle.dot(1, (pixel / 255, pixel / 255, pixel / 255))
+        turtle.update()
 
 def big_fernand(pathFile):
     image = cv2.imread(pathFile)
+    rows, columns, channels = image.shape
+    turtle.setup(columns, rows)
+    turtle.pensize(10)
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     median_pix = np.median(gray_image)
     lower = int(max(0 ,0.25*median_pix))
     upper = int(min(255,0.75*median_pix))
     edges = cv2.Canny(image, threshold1=lower,threshold2=upper)
     ret, thresh = cv2.threshold(edges, 127, 255, cv2.THRESH_BINARY_INV)
-    cv2.imwrite('etape2.jpg', thresh)
+    for y in range(rows):
+      for x in range(columns):
+        pixel = thresh[y, x]
+        turtle.penup()
+        turtle.goto(x, rows - y)
+        turtle.pendown()
+        turtle.dot(1, (pixel / 255, pixel / 255, pixel / 255))
+        turtle.update()
     
 def main():
     if len(sys.argv) < 2:
