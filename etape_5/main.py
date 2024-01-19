7c308d3ae2e107d9bb7df65171218d55c3c0c0d5
aggoun_r <aggoun_r@etna-alternance.net>
2024-01-20 00:53:18 +0100

    Update main.py

--- etape_5/main.py
+++ etape_5/main.py
@@ -1,1 +1,54 @@
+import cv2
+import numpy as np
+import argparse
+from my_turtle import MyTurtle
+from setting import Setting
 
+
+def big_fernand(s):
+    if s.blur > 0:
+        intensity = s.blur
+        if intensity % 2 == 0:
+            intensity += 1
+        if s.blur_type == "gaussian":
+            s.image = cv2.GaussianBlur(s.image, (intensity, intensity), 0)
+        elif s.blur_type == "box":
+            s.image = cv2.blur(s.image, (intensity, intensity))
+        elif s.blur_type == "bilateral":
+            s.image = cv2.bilateralFilter(s.image, 9, intensity, intensity)
+    gray_image = cv2.cvtColor(s.image, cv2.COLOR_BGR2GRAY)
+    median_pix = np.median(gray_image)
+    lower = int(max(0, 0.25 * median_pix))
+    upper = int(min(255, 0.75 * median_pix))
+    edges = cv2.Canny(s.image, threshold1=lower, threshold2=upper)
+    ret, thresh = cv2.threshold(edges, 127, 255, cv2.THRESH_BINARY_INV)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    formatted_contours = []
+    for contour in contours:
+        c = contour.squeeze()
+        if c.ndim != 2:
+            c = c.reshape(1, 2)
+        formatted_contours.append(c)
+
+    my_turtle = MyTurtle(thresh, formatted_contours)
+    my_turtle.set_tracer_active(s.update_value)
+    my_turtle.hide_turtle()
+
+    if s.mode == 1:  # imprimer le dessin
+        my_turtle.print(s.image)
+    else:  # dessiner
+        my_turtle.draw(s.image, formatted_contours)
+
+
+def main():
+    console = argparse.ArgumentParser()
+    console.add_argument('--path', type=str)
+    console.add_argument('--update', type=int, default=10)
+    console.add_argument('--blur', type=int, default=0)
+    console.add_argument('--blur_type', type=str, choices=['box', 'gaussian', 'bilateral'], default='box')
+    console.add_argument('--mode', type=str, choices=['Draw', 'Print'], default='Draw')
+    args = console.parse_args()
+    big_fernand(Setting(args))
+
+
+main()
