c662722a099c2527c97e96cb2f22dee24fd24d9c
aggoun_r <aggoun_r@etna-alternance.net>
2024-01-21 23:36:00 +0100

    Update setting.py

--- etape_5/setting.py
+++ etape_5/setting.py
@@ -8,6 +8,8 @@ class Setting:
         self.image = cv2.imread(arguments.path)
         self.mode = arguments.mode
         self.blur_type = arguments.blur_type
+        self.number_colors = arguments.number_colors
+        self.path = arguments.path
 
         if self.image is None:
             print(f"Erreur : Ouverture du fichier impossible, vérifier le chemin")
@@ -16,11 +18,14 @@ class Setting:
         try:
             upper_than_exception(self.blur, 'blur')
             upper_than_exception(self.update_value, 'update')
+            upper_than_exception(self.number_colors, 'number_colors',1)
         except ValueError as e:
             print(str(e))
             sys.exit(1)
 
 
 def upper_than_exception(value, name, limit=0):
-    if value < limit:
+    if value < limit and limit == 0:
         raise ValueError(f"Erreur: '{name}' doit être un entier supérireur ou égale à 0")
+    elif value < limit and limit == 1:
+        raise ValueError(f"Erreur: '{name}' doit être un entier strictement supérireur à 0")
